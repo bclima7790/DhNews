4c286eff498085d89d5646c589dc418cd84178b4
Bruno Lima <bclima7790@gmail.com>
2022-07-02 13:03:11 -0300

    Envio do conteúdo de html da página para a rota da home

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,14 @@
-let express = require('express');
-let server = express();
+const express = require('express');
+const server = express();
+const path = require('path');
+
 
 server.get("/", (req, res) => {
-    res.send('Homepage');
+    res.sendFile(path.join(__dirname,'../Public/index.html'));
 }) //Envio de conteúdo da rota da homepage
+
 server.listen(3000, () => {
-    console.log('Servidor funcionando!')
+    console.log('Servidor funcionando!');
 }) //Lançamento do servidor
 
 
